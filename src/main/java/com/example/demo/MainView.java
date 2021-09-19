839193d9fd7cc80b33688707cd0bf1ae1216c292
Julien Helbling <Julien.Helbling@apgsga.ch>
2021-09-19 21:31:51 +0200

    scrollable content

--- src/main/java/com/example/demo/MainView.java
+++ src/main/java/com/example/demo/MainView.java
@@ -1,39 +1,20 @@
 package com.example.demo;
 
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.contextmenu.MenuItem;
-import com.vaadin.flow.component.contextmenu.SubMenu;
+import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.menubar.MenuBar;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLayout;
-import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.server.VaadinService;
 
-import java.util.Locale;
-
-/**
- * The main view contains a button and a click listener.
- */
-//@Route("")
 @CssImport("./styles/shared-styles.css")
-//@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
-public class MainView extends VerticalLayout implements RouterLayout {
+public class MainView extends AppLayout implements RouterLayout {
 
     public MainView() {
-
         Label mainTitle = new Label("Association de solidarité Kurumbagaram");
         mainTitle.setId("mainTitle");
-        add(mainTitle);
-        setAlignItems(Alignment.CENTER);
-        add(new MainMenuBar());
-        add(new LanguageList().getLanguageList());
+        VerticalLayout vl = new VerticalLayout(mainTitle,new MainMenuBar(), new LanguageList().getLanguageList());
+        vl.setAlignItems(FlexComponent.Alignment.CENTER);
+        addToNavbar(vl);
     }
 }
