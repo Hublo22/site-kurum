1d6275633bac82dcb28008184ea8a29c8dae6f4d
Julien Helbling <Julien.Helbling@apgsga.ch>
2021-09-22 20:39:35 +0200

    cleaning up

--- src/main/java/com/example/demo/MainMenuBar.java
+++ src/main/java/com/example/demo/MainMenuBar.java
@@ -17,39 +17,9 @@ public class MainMenuBar extends MenuBar implements LocaleChangeObserver {
     public MainMenuBar() {
         super();
 
-//        MenuItem presentation = addItem(getTranslation("presentation", UI.getCurrent().getLocale()));
-//        presentation.addClickListener(e -> UI.getCurrent().navigate("presentation"));
-
-
         MenuItem comite = addItem(getTranslation("comite", UI.getCurrent().getLocale()));
         comite.setId("comite");
         comite.addClickListener(e -> UI.getCurrent().navigate("comite"));
-/*
-        MenuItem projects = addItem(getTranslation("projects", UI.getCurrent().getLocale()));
-        projects.setId("projects");
-        projects.addClickListener(e -> UI.getCurrent().navigate("projects"));
-
-        MenuItem contact = addItem(getTranslation("contact", UI.getCurrent().getLocale()));
-        contact.setId("contact");
-        contact.addClickListener(e -> UI.getCurrent().navigate("contact"));
-
-        MenuItem history = addItem(getTranslation("history", UI.getCurrent().getLocale()));
-        history.setId("history");
-        history.addClickListener(e -> UI.getCurrent().navigate("history"));
-
-        MenuItem sisters = addItem(getTranslation("sisters", UI.getCurrent().getLocale()));
-        sisters.setId("sisters");
-        sisters.addClickListener(e -> UI.getCurrent().navigate("sisters"));
-
-        MenuItem home = addItem(getTranslation("home", UI.getCurrent().getLocale()));
-        home.setId("home");
-        home.addClickListener(e -> UI.getCurrent().navigate(""));
-
-        MenuItem helpus = addItem(getTranslation("helpus", UI.getCurrent().getLocale()));
-        helpus.setId("helpus");
-        helpus.addClickListener(e -> UI.getCurrent().navigate("helpus"));
-
- */
     }
 
 
